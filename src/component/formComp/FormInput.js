287843a2907b808841b0e4a39de943865c59922e
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-04-11 00:40:42 +0530

    form comp created, onChange function created

--- src/component/formComp/FormInput.js
+++ src/component/formComp/FormInput.js
@@ -1,10 +1,52 @@
 import React from 'react';
 
-const FormInput = () => {
+const FormInput = ({ handleChange }) => {
   return (
     <form>
-      <input type='text' name='name' className='name' />
-      <input type='text' name='cardNum' className='number' />
+      <div>
+        {' '}
+        <label>CARDHOLDER NAME</label>
+        <input
+          onChange={handleChange}
+          type='text'
+          name='name'
+          className='name'
+        />
+        <label>CARD NUMBER</label>
+        <input
+          onChange={handleChange}
+          type='text'
+          name='cardNum'
+          className='number'
+        />
+      </div>
+      <div>
+        <div>
+          {' '}
+          <label>EXP.DATE (MM/YY)</label>
+          <input
+            onChange={handleChange}
+            type='text'
+            name='day'
+            className='day'
+          />
+          <input
+            onChange={handleChange}
+            type='text'
+            name='month'
+            className='month'
+          />
+        </div>
+        <div>
+          <label>CVC</label>
+          <input
+            onChange={handleChange}
+            type='number'
+            name='cvc'
+            className='cvc'
+          />
+        </div>
+      </div>
     </form>
   );
 };
--- src/component/mainCnt/Container.js
+++ src/component/mainCnt/Container.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import FormInput from '../formComp/FormInput';
 
 const Container = () => {
+  const [formData, setFormData] = useState();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  console.log(formData);
+
   return (
     <>
       <div>
         <div></div>
         <div>
-          <FormInput />
+          <FormInput handleChange={handleChange} />
         </div>
       </div>
     </>
