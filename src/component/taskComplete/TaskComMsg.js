3e92b5610e5d356fb21b60a915fda66401e29003
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-04-18 11:30:39 +0530

    style added

--- src/component/taskComplete/TaskComMsg.js
+++ src/component/taskComplete/TaskComMsg.js
@@ -11,7 +11,7 @@ const TaskComMsg = ({ setIsTask }) => {
           width='36'
           height='36'
           fill='currentColor'
-          className='bi bi-check2'
+          className=' bi-check2'
           viewBox='0 0 16 16'
         >
           <path d='M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z' />
